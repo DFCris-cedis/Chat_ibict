af89ae482cca2d6423ddece5382463dc2ea7343f
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2023-05-01 22:52:55 -0300

    clean middleware and form

--- Progressao/paginas/forms.py
+++ Progressao/paginas/forms.py
@@ -1,9 +1,8 @@
-from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 from django import forms
 from .models import Test
-from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.models import User
+
 
 
 class MeuForm(forms.ModelForm):
