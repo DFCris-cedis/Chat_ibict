6437794fbff69b80efa04b62f11edcafd17eeeda
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2023-06-28 01:13:24 -0300

    Update views.py

--- Progressao/paginas/views.py
+++ Progressao/paginas/views.py
@@ -289,7 +289,7 @@ def get_df():
         cursor.close()
         
         # file = open("C:/Users/milen/OneDrive/Documentos/DF/todos_IDSignificados.Ocorrencias.csv", "r")
-        file = open(r"home\ubuntu\Chat_ibict\Progressao\static\modelos\todos_IDSignificados.Ocorrencias.csv", "r")
+        file = open("/home/ubuntu/Chat_ibict/Progressao/static/modelos/todos_IDSignificados.Ocorrencias.csv", "r")
 
 
         idsignificado = list(csv.reader(file, delimiter=","))
