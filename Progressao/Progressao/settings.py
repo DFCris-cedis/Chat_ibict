1bd853e4df61344f8c8e7ed24f85f56212336fd9
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2023-09-10 14:24:18 -0300

    .

--- Progressao/Progressao/settings.py
+++ Progressao/Progressao/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-!ph7usvbsvm2k#f@%!%v=$ggv*s-f=&1(@rakrh+czsv*$6c9*
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['*']
+#CSRF_TRUSTED_ORIGINS = ['*']
 
 
 # Application definition
