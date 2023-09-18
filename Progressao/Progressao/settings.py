b8e2c7bddc42478be35ce553fb1186ec8f5cab82
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2023-09-17 21:53:31 -0300

    reset password

--- Progressao/Progressao/settings.py
+++ Progressao/Progressao/settings.py
@@ -185,8 +185,8 @@ LOGOUT_REDIRECT_URL = 'login'
 # Configurações de email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_PORT = 465  # Altere a porta para 587
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587  # Altere a porta para 587
 EMAIL_HOST_USER = 'milenafaria1706@gmail.com'
 EMAIL_HOST_PASSWORD = 'ckwktyijrxlltfjo'
 
