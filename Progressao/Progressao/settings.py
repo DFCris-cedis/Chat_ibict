3fb8f5a3dcf9e10045d54e3890945421a874c96d
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2023-10-02 00:44:30 -0300

    .

--- Progressao/Progressao/settings.py
+++ Progressao/Progressao/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 # settings.py
 
-ALLOWED_HOSTS = ['contextus.ibict.br', '200.130.0.2','172.16.17.41']
+ALLOWED_HOSTS = ['http://contextus.ibict.br', '200.130.0.2','172.16.17.41']
 CSRF_TRUSTED_ORIGINS = ['http://contextus.ibict.br']
 
 
