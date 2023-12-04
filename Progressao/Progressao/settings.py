f10138e253b0cf15d91f9a5e0b1e06a986efe3f0
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2023-12-04 17:05:26 -0300

    .

--- Progressao/Progressao/settings.py
+++ Progressao/Progressao/settings.py
@@ -193,7 +193,7 @@ LOGOUT_REDIRECT_URL = 'login'
 # Configurações de email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Replace with your SMTP server address
-EMAIL_PORT = 587  # Replace with your SMTP server port
+EMAIL_PORT = 25  # Replace with your SMTP server port
 EMAIL_USE_TLS = True  # Use TLS for secure communication
 EMAIL_HOST_USER = 'ibictcontextus@gmail.com'
 EMAIL_HOST_PASSWORD = 'rvdr bagw boai wzvh'
