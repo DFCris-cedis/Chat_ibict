ef5e535ec51452c08fc085bb7520bb05223c5baf
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2023-12-04 18:15:48 -0300

    .

--- Progressao/paginas/urls.py
+++ Progressao/paginas/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('reset_password_form/', auth_views.PasswordResetForm.as_view(), name='password_reset_form'),
     path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('/reset_password_confirm/<uidb64>/<token>/', MyPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_confirm/<uidb64>/<token>/', MyPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_complete'),
 
 ]
