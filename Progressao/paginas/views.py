c1a9e2ab77c549ca88f3cfa8e75aae608e65b188
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2024-01-23 00:04:39 -0300

    diretory df

--- Progressao/paginas/views.py
+++ Progressao/paginas/views.py
@@ -1,6 +1,6 @@
 import sys
-#sys.path.append('home/milenasilva/Chat_ibict/Progressao/')
-sys.path.append('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao')
+sys.path.append('home/milenasilva/Chat_ibict/Progressao/')
+#sys.path.append('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao')
 
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
@@ -195,7 +195,9 @@ def get_df():
         # Fecha o cursor e a conexão
         cursor.close()
         
-        file = open("C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/modelos/todos_IDSignificados.Ocorrencias.csv", "r")
+        #file = open("C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/todos_IDSignificados.Ocorrencias.csv", "r")
+        file = open("home/milenasilva/Chat_ibict/Progressao/static/Modelos/todos_IDSignificados.Ocorrencias.csv", "r")
+       
         idsignificado = list(csv.reader(file, delimiter=","))
         file.close()
 
@@ -229,8 +231,8 @@ warnings.simplefilter("ignore")
 def prevNN(abstract):
     localH2o = h2o.init(nthreads=-1)
     
-    Modelo = h2o.load_model("C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/DeepLearning_model_R_1670582405235_1")
-    #Modelo = h2o.load_model("/home/milenasilva/Chat_ibict/Progressao/Progressao/Modelos/DeepLearning_model_R_1670582405235_1")
+    #Modelo = h2o.load_model("C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/DeepLearning_model_R_1670582405235_1")
+    Modelo = h2o.load_model("/home/milenasilva/Chat_ibict/Progressao/static/Modelos/DeepLearning_model_R_1670582405235_1")
     
     prevNN = Modelo.predict(h2o.H2OFrame(abstract))
     
@@ -253,41 +255,59 @@ def process():
     print(area)
 
     if area == 'CIENCIAS SOCIAIS APLICADAS':
-        path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/CienciasSociaisAplicadas_xgboost.pkl' 
-        encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/encoder_CienciasSociaisAplicadas.csv')
+        # path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/CienciasSociaisAplicadas_xgboost.pkl' 
+        # encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/encoder_CienciasSociaisAplicadas.csv')
+        path_model = '/home/milenasilva/Chat_ibict/Progressao/static/Modelos/CienciasSociaisAplicadas_xgboost.pkl' 
+        encoder = pd.read_csv('/home/milenasilva/Chat_ibict/Progressao/static/Modelos/encoder_CienciasSociaisAplicadas.csv')
 
     if area == 'CIENCIAS DA SAUDE':
-        path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/CienciasDaSaude_xgboost.pkl'
-        encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/encoder_CienciasDaSaude.csv')
+        # path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/CienciasDaSaude_xgboost.pkl'
+        # encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/encoder_CienciasDaSaude.csv')
+        path_model = '/home/milenasilva/Chat_ibict/Progressao/static/Modelos/CienciasDaSaude_xgboost.pkl'
+        encoder = pd.read_csv('/home/milenasilva/Chat_ibict/Progressao/static/Modelos/encoder_CienciasDaSaude.csv')
         
     if area == 'LINGUISTICA, LETRAS E ARTES':
-        path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/LinguisticaLetrasArtes_xgboost.pkl' 
-        encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/encoder_LinguisticaLetrasArtes.csv')
+        # path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/LinguisticaLetrasArtes_xgboost.pkl' 
+        # encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/encoder_LinguisticaLetrasArtes.csv')
+         path_model = '/home/milenasilva/Chat_ibict/Progressao/static/Modelos/LinguisticaLetrasArtes_xgboost.pkl' 
+         encoder = pd.read_csv('/home/milenasilva/Chat_ibict/Progressao/static/Modelos/encoder_LinguisticaLetrasArtes.csv')
         
     if area == 'CIENCIAS EXATAS E DA TERRA':
-        path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/CienciasExatasDaTerra_xgboost.pkl' 
-        encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/encoder_CienciasExatasDaTerra.csv')
+        # path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/CienciasExatasDaTerra_xgboost.pkl' 
+        # encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/encoder_CienciasExatasDaTerra.csv')
+        path_model = '/home/milenasilva/Chat_ibict/Progressao/static/Modelos/CienciasExatasDaTerra_xgboost.pkl' 
+        encoder = pd.read_csv('/home/milenasilva/Chat_ibict/Progressao/static/Modelos/encoder_CienciasExatasDaTerra.csv')
         
     if area == 'MULTIDISCIPLINAR':
-        path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/Multidisciplinar_xgboost.pkl' 
-        encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/encoder_Multidisciplinar.csv')
+        # path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/Multidisciplinar_xgboost.pkl' 
+        # encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/encoder_Multidisciplinar.csv')
+        path_model = '/home/milenasilva/Chat_ibict/Progressao/static/Modelos/Multidisciplinar_xgboost.pkl' 
+        encoder = pd.read_csv('/home/milenasilva/Chat_ibict/Progressao/static/Modelos/encoder_Multidisciplinar.csv')
         
     if area == 'CIENCIAS HUMANAS':
-        path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/CienciasHumanas_xgboost.pkl' 
-        encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/encoder_CienciasHumanas.csv')
+        # path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/CienciasHumanas_xgboost.pkl' 
+        # encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/encoder_CienciasHumanas.csv')
+        path_model = '/home/milenasilva/Chat_ibict/Progressao/static/Modelos/CienciasHumanas_xgboost.pkl' 
+        encoder = pd.read_csv('/home/milenasilva/Chat_ibict/Progressao/static/Modelos/encoder_CienciasHumanas.csv')
         
     if area == 'ENGENHARIAS':
-        path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/Engenharias_xgboost.pkl' 
-        encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/encoder_Engenharias.csv')
+        # path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/Engenharias_xgboost.pkl' 
+        # encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/encoder_Engenharias.csv')
+        path_model = '/home/milenasilva/Chat_ibict/Progressao/static/Modelos/Engenharias_xgboost.pkl' 
+        encoder = pd.read_csv('/home/milenasilva/Chat_ibict/Progressao/static/Modelos/encoder_Engenharias.csv')
         
     if area == 'CIENCIAS BIOLOGICAS':
-        path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/CienciasBiologicas_xgboost.pkl' 
-        encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/encoder_CienciasBiologicas.csv')
+        # path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/CienciasBiologicas_xgboost.pkl' 
+        # encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/encoder_CienciasBiologicas.csv')
+        path_model = '/home/milenasilva/Chat_ibict/Progressao/static/Modelos/CienciasBiologicas_xgboost.pkl' 
+        encoder = pd.read_csv('/home/milenasilva/Chat_ibict/Progressao/static/Modelos/encoder_CienciasBiologicas.csv')
         
     if area == 'CIENCIAS AGRARIAS':
-        path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/CienciasAgrarias_xgboost.pkl' 
-        encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/Progressao/Modelos/encoder_CienciasAgrarias.csv')
+        # path_model = 'C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/CienciasAgrarias_xgboost.pkl' 
+        # encoder = pd.read_csv('C:/Users/milen/OneDrive/Documentos/GitHub/Chat_ibict/Progressao/static/Modelos/encoder_CienciasAgrarias.csv')
 
+        path_model = '/home/milenasilva/Chat_ibict/Progressao/static/Modelos/CienciasAgrarias_xgboost.pkl' 
+        encoder = pd.read_csv('/home/milenasilva/Chat_ibict/Progressao/static/Modelos/encoder_CienciasAgrarias.csv')
     
     sub = ''
 
